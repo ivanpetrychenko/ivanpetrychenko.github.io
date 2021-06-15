e0977cf7dc74dc513719fd321e607e368b0f2f49
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2021-06-15 16:52:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.querySelector('.btn').addEventListener('click', () => {
+    console.log(1);
+    window.navigator.vibrate(200);
+
+    // console.log(1);
+    // navigator.getBattery()
+    //     .then(battery => {
+    //         console.log(battery.level * 100 + "%")
+    //     })
+});
