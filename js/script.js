95bcd25c9904f974998b61544b845ac4d0c6739c
Ivan Petrychenko <ivanpetrychenko95@gmail.com>
2021-06-15 17:02:43 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ document.querySelector('.btn').addEventListener('click', () => {
     // console.log(1);
     // window.navigator.vibrate(200);
 
+    // console.log(1);
+    // navigator.getBattery()
+    //     .then(battery => {
+    //         // console.log(battery.level * 100 + "%");
+    //         document.querySelector('.text').innerHTML = battery.level * 100 + "%";
+    //     })
+
     console.log(1);
-    navigator.getBattery()
-        .then(battery => {
-            // console.log(battery.level * 100 + "%");
-            document.querySelector('.text').innerHTML = battery.level * 100 + "%";
-        })
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(pos => document.querySelector('.text').innerHTML = pos.coords.latitude);
+    }
 });
